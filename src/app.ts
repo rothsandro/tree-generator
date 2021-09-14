a64b90b4d85c1f3a75415a8275954deee31123be
Sandro Roth <sandroroth@outlook.com>
2021-09-14 19:33:11 +0200

    feat: add textarea for input/output

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import { parseInput } from "./core/parser";
 import Alpine from "alpinejs";
 
-Alpine.data("tree", () => {
+Alpine.data("treeGenerator", () => {
   const input = "src/\nfile.txt";
   const output = parseInput(input);
-  return { output };
+  const formattedOutput = JSON.stringify(output, null, 2);
+  return { input, output, formattedOutput };
 });
 
 Alpine.start();
