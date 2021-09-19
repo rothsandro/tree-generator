fb3d2636b7cda70b4810a00e57f43aec5bf368f9
Sandro Roth <sandroroth@outlook.com>
2021-09-19 14:49:02 +0200

    feat: add example as default value

--- src/config/default-values.ts
+++ src/config/default-values.ts
@@ -2,11 +2,33 @@ import { TreeConfig } from "../types/config.types";
 
 export function getDefaultTreeConfig(): TreeConfig {
   return {
-    rootElement: false,
+    rootElement: true,
     alignComments: true,
   };
 }
 
 export function getDefaultInput(): string {
-  return `src\n  file.txt`;
+  return [
+    "Generate a nice folder tree",
+    "by editing this example.",
+    "",
+    "Use indentation to create",
+    "nested folders and files.",
+    'You can use "#” at the end',
+    "of a line to add a comment.",
+    "",
+    "Here is an example:",
+    "",
+    "src",
+    "  assets # Images, fonts and more",
+    "    hero.png",
+    "    logo.png",
+    "  modules",
+    "   feature-a # Each feature is a module",
+    "     index.ts # Some code",
+    "   feature-b",
+    "     index.ts",
+    "  App.test.tsx",
+    "  App.ts",
+  ].join("\n");
 }
